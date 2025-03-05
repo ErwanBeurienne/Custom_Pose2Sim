7892b17910c50343eebf1e7144c243648e52a6d5
ErwanBeurienne <131443612+ErwanBeurienne@users.noreply.github.com>
2025-03-05 17:23:12 -0500

    Enhance Pre_Processing.py with logging, type hints, and improved error handling; refactor functions for better clarity and structure

--- Run_P2S/Pre_Processing.py
+++ Run_P2S/Pre_Processing.py
@@ -4,34 +4,54 @@ import ffmpeg
 import shutil
 import pandas as pd
 from datetime import datetime
+import logging
+from typing import Tuple, Optional, Union
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def import_log_test(log_test_path):
-    """ function doc """
-    
-    # Load log test file
-    test_log_file_df = pd.read_excel(
-        log_test_path,
-        header = 0,
-        usecols = ["Groups", "Trials", "Date", "Time", "Athlete ID"],
-        dtype ={"Athlete ID": str}
+def import_log_test(log_test_path: str) -> pd.DataFrame:
+    """Load the log test file from the specified path.
+
+    Args:
+        log_test_path (str): Path to the log test Excel file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the log test data.
+    """
+    try:
+        # Read the Excel file and specify that 'Athlete ID' should be read as a string
+        test_log_file_df = pd.read_excel(
+            log_test_path,
+            header=0,
+            usecols=["Groups", "Trials", "Date", "Time", "Athlete ID"],
+            dtype={"Athlete ID": str}
         )
-    
-    return test_log_file_df
+        return test_log_file_df
+    except Exception as e:
+        logging.error(f"Error reading Excel file: {e}")
+        raise
 
+def create_folder_structure(batch_path: str, trial_name: Optional[str] = None) -> Union[str, Tuple[str, str]]:
+    """Create the folder structure for a BatchSession and a trial.
 
-def create_folder_structure(batch_path, trial_name=None):
-    """Crée la structure de dossiers pour une BatchSession et un essai."""
+    Args:
+        batch_path (str): Path to the batch session folder.
+        trial_name (Optional[str]): Name of the trial folder.
 
+    Returns:
+        Union[str, Tuple[str, str]]: Path to the trial folder or paths to the calibration folders.
+    """
+    # Create the batch session folder if it doesn't exist
     os.makedirs(batch_path, exist_ok=True)
 
     if trial_name:
+        # Create the trial folder inside the batch session folder
         trial_path = os.path.join(batch_path, trial_name)
         os.makedirs(trial_path, exist_ok=True)
-        
         return trial_path
-    
     else:
+        # Create the calibration folders inside the batch session folder
         intrinsics_calibration_path = os.path.join(batch_path, "calibration", "intrinsics")
         os.makedirs(intrinsics_calibration_path, exist_ok=True)
         
@@ -40,131 +60,174 @@ def create_folder_structure(batch_path, trial_name=None):
         
         return intrinsics_calibration_path, extrinsics_calibration_path
 
+def paste_config_file(folder_path: str) -> None:
+    """Copy the Config.toml file to the specified folder.
 
-def paste_config_file(folder_path):
-    """Copie le fichier Config.toml (qui est dans le même dossier que ce script) vers le dossier spécifié."""
-    
+    Args:
+        folder_path (str): Destination folder path.
+    """
+    # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))  
+    # Path to the source Config.toml file
     source_config_path = os.path.join(script_dir, "Config.toml")
 
     if not os.path.exists(source_config_path):
-        print(f"The file {source_config_path} doesn't exist!")
+        logging.warning(f"The file {source_config_path} doesn't exist!")
         return
 
+    # Path to the destination Config.toml file
     destination_config_path = os.path.join(folder_path, "Config.toml")
+    # Copy the Config.toml file to the destination folder
     shutil.copy(source_config_path, destination_config_path)
 
+def convert_to_local_time(datetime_obj: datetime, target_timezone: str = "America/Montreal", adjust_time: bool = False) -> datetime:
+    """Convert a naive datetime to local time (Montreal) with optional time adjustment.
 
-def convert_to_local_time(datetime_obj, target_timezone="America/Montreal", adjust_time=False):
-    """Convertit un datetime naïf en heure locale (Montréal) avec possibilité d'adaptation de l'heure."""
-    
+    Args:
+        datetime_obj (datetime): Naive datetime object.
+        target_timezone (str): Target timezone.
+        adjust_time (bool): Whether to adjust the time.
+
+    Returns:
+        datetime: Localized datetime object.
+    """
+    # Get the target timezone
     local_tz = pytz.timezone(target_timezone)
-    
+    # Localize the naive datetime object to the target timezone
     datetime_local = local_tz.localize(datetime_obj)
 
     if adjust_time:
+        # Convert the naive datetime object to UTC and then to the target timezone
         utc_tz = pytz.utc
-        datetime_obj_utc = utc_tz.localize(datetime_obj)  # Localiser l'heure naïve en UTC
+        datetime_obj_utc = utc_tz.localize(datetime_obj)
         datetime_local = datetime_obj_utc.astimezone(local_tz)
 
     return datetime_local
-    
 
-def get_video_creation_time(video_path):
-    """Récupère l'heure de création d'une vidéo"""
-    
+def get_video_creation_time(video_path: str) -> Optional[datetime]:
+    """Get the creation time of a video.
+
+    Args:
+        video_path (str): Path to the video file.
+
+    Returns:
+        Optional[datetime]: Creation time of the video.
+    """
     try:
-        probe = ffmpeg.probe(video_path, v='error', select_streams='v:0', show_entries='format_tags=creation_time') # Récupérer les informations sur la vidéo via ffmpeg
-        creation_time_str = probe['format']['tags']['creation_time'] # Extraire la date de création
-        creation_time = datetime.strptime(creation_time_str, '%Y-%m-%dT%H:%M:%S.%fZ') # Convertir en objet datetime
-
+        # Use ffmpeg to probe the video file and get the creation time
+        probe = ffmpeg.probe(video_path, v='error', select_streams='v:0', show_entries='format_tags=creation_time')
+        creation_time_str = probe['format']['tags']['creation_time']
+        # Convert the creation time string to a datetime object
+        creation_time = datetime.strptime(creation_time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
         return creation_time
-    
     except ffmpeg.Error as e:
-        print(f"Erreur ffmpeg: {e}")
+        logging.error(f"ffmpeg error: {e}")
         return None
 
+def rename_video_extension(video_path: str) -> str:
+    """Rename the extension of a video file to .mp4.
 
-def rename_video_extension(video_path):
-    """Renomme l'extension d'un fichier vidéo en .mp4, peu importe la casse de l'extension d'origine."""
-    
-    new_video_name = os.path.splitext(video_path)[0] + ".mp4" # Crée le nouveau nom avec l'extension .mp4
-    os.rename(video_path, new_video_name) # Renomme le fichier
+    Args:
+        video_path (str): Path to the video file.
+
+    Returns:
+        str: New video file path with .mp4 extension.
+    """
+    # Change the extension of the video file to .mp4
+    new_video_name = os.path.splitext(video_path)[0] + ".mp4"
+    os.rename(video_path, new_video_name)
     return new_video_name
 
+def move_videos(source_folder: str, destination_folder: str, target_time: datetime, trial_type: str) -> None:
+    """Move videos from camera subfolders to the destination folder.
 
-def move_videos(source_folder, destination_folder, target_time, trial_type):
-    """Déplace les vidéos des sous-dossiers des caméras vers le dossier de destination."""
-    
-    closest_time_diff = None
+    Args:
+        source_folder (str): Source folder path.
+        destination_folder (str): Destination folder path.
+        target_time (datetime): Target time for video selection.
+        trial_type (str): Type of trial (e.g., "calibration").
+    """
+    # Convert the target time to local time
     target_time = convert_to_local_time(target_time)
-
+    # Get the list of camera folders in the source folder
     camera_folders = [f for f in os.listdir(source_folder) if "cam" in f.lower() and os.path.isdir(os.path.join(source_folder, f))]
-    
+
     for camera_folder in camera_folders:
         camera_path = os.path.join(source_folder, camera_folder)
+        # Get the list of video files in the camera folder
         video_files = [f for f in os.listdir(camera_path) if f.lower().endswith('.mp4')]
         closest_time_diff = None
-        
+        closest_video = None
+
         for video_file in video_files:
             video_path = os.path.join(camera_path, video_file)
+            # Get the creation time of the video
             creation_time = get_video_creation_time(video_path)
-            creation_time = convert_to_local_time(creation_time, adjust_time=True)
-            time_diff = abs((creation_time - target_time).total_seconds())
-            
-            if closest_time_diff is None or time_diff < closest_time_diff:
-                closest_time_diff = time_diff
-                closest_video = video_path
-        
+            if creation_time:
+                # Convert the creation time to local time
+                creation_time = convert_to_local_time(creation_time, adjust_time=True)
+                # Calculate the time difference between the creation time and the target time
+                time_diff = abs((creation_time - target_time).total_seconds())
+
+                # Find the video with the closest creation time to the target time
+                if closest_time_diff is None or time_diff < closest_time_diff:
+                    closest_time_diff = time_diff
+                    closest_video = video_path
+
         if closest_video:
+            # Rename the video file to have a .mp4 extension
             closest_video_renamed = rename_video_extension(closest_video)
-            
+            # Determine the destination path based on the trial type
             if trial_type == "calibration":
                 destination_path = os.path.join(destination_folder, f"ext_{camera_folder}", f"ext_{camera_folder}.mp4")
             else:
                 destination_path = os.path.join(destination_folder, "videos", f"{camera_folder}.mp4")
-            
+
+            # Create the destination folder if it doesn't exist
             os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+            # Copy the video file to the destination folder
             shutil.copy(closest_video_renamed, destination_path)
-            print(f"Video {closest_video_renamed} moved to {destination_path}")
+            logging.info(f"Video {closest_video_renamed} moved to {destination_path}")
 
+def organize_videos(excel_file_path: str, source_folder: str, destination_folder: str) -> None:
+    """Organize videos according to the structure required by Pose2Sim.
 
-def organize_videos(excel_file_path, source_folder, destination_folder):
-    """Organise les vidéos selon la structure demandée par Pose2Sim."""
-    # Lire le fichier Excel
+    Args:
+        excel_file_path (str): Path to the Excel file containing the log test data.
+        source_folder (str): Source folder path.
+        destination_folder (str): Destination folder path.
+    """
+    # Import the log test data from the Excel file
     test_log_file = import_log_test(excel_file_path)
-
-    working_date = None
+    current_date = None
 
     for index, row in test_log_file.iterrows():
-
         trial_date = row["Date"].strftime("%Y-%m-%d")
         trial_type = row["Trials"]
         athlete_ID = row["Athlete ID"]
         trial_date_time = datetime.combine(row["Date"], row["Time"])
 
-        if trial_date != working_date:
-            working_date = trial_date
+        if trial_date != current_date:
+            current_date = trial_date
+            # Create the base path for the session
             base_path = os.path.join(destination_folder, f"Session_{trial_date}")
             os.makedirs(base_path, exist_ok=True)
             batch_counter = 1
             trial_counters = {}
 
         if trial_type == "calibration":
+            # Create the folder structure for a calibration batch session
             batch_session = f"BatchSession_{batch_counter}"
             batch_path = os.path.join(base_path, batch_session)
             _, extrinsics_calibration_path = create_folder_structure(batch_path)
-            
             paste_config_file(batch_path)
             move_videos(source_folder, extrinsics_calibration_path, trial_date_time, trial_type)
-            
             batch_counter += 1
-        
         else:
-            trial_name = f"Trial_{athlete_ID}_{trial_counters.get(athlete_ID, 0) + 1}"
-            trial_path = create_folder_structure(batch_path, trial_name)
-            
+            # Create the folder structure for a trial
+            trial_counter = trial_counters.get(athlete_ID, 0) + 1
+            trial_counters[athlete_ID] = trial_counter
+            trial_name = f"Trial_{athlete_ID}_{trial_counter}"
+            trial_path = create_folder_structure(base_path, trial_name)
             paste_config_file(trial_path)
             move_videos(source_folder, trial_path, trial_date_time, trial_type)
-
-            trial_counters[athlete_ID] = trial_counters.get(athlete_ID, 0) + 1
